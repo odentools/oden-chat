818b91a3e275c8ada5309955e8dd354c553eb0b3
DaikiYano <ht12a092@gmail.com>
2016-06-11 05:43:14 +0900

    Refactor: コメントの追加と処理の処理の順序の変更．

--- server.js
+++ server.js
@@ -8,147 +8,166 @@ app.use(express.static('www'));
 
 var iSeeCount = 0;
 var hashCount = 0;
-var pdeFile = [], pdeImgFiles = [];
+var pdeFile = [],
+    pdeImgFiles = [];
 
 var infoStatus = "off";
 var serviceLock = false;
 var headerMsg = "Ｃ＋＋１　Processingで作った，みんなのプログラムをドラッグ・アンド・ドロップしてみよう！";
 
-app.get('/*.jpg', function(req, res){
-	res.end();
+app.get('/*.jpg', function(req, res) {
+    res.end();
 });
 
-app.get('/pde/:id', function(req, res){
-	res.send(pdeFile[req.params.id]);
+app.get('/pde/:id', function(req, res) {
+    res.send(pdeFile[req.params.id]);
 });
 
-app.get('/admin/dumpPdeFiles', function(req, res){
-	res.json(pdeFile);
+app.get('/admin/dumpPdeFiles', function(req, res) {
+    res.json(pdeFile);
 });
 
-app.get('/admin/info/:status', function(req, res){
-	if (req.params.status == "on") {
-		io.emit('info', "on");
-		infoStatus = "on";
-	} else if (req.params.status == "off") {
-		io.emit('info', "off");
-		infoStatus = "off";
-	}
+app.get('/admin/info/:status', function(req, res) {
+    if (req.params.status == "on") {
+        io.emit('info', "on");
+        infoStatus = "on";
+    } else if (req.params.status == "off") {
+        io.emit('info', "off");
+        infoStatus = "off";
+    }
 
-	res.end();
-
-});
-
-app.get('/admin/serviceLock/:status', function(req, res){
-	if (req.params.status == "on") {
-		serviceLock = true;
-	} else if (req.params.status == "off") {
-		serviceLock = false;
-	}
-
-	res.end();
+    res.end();
 
 });
 
-app.get('/admin/headerMsg/:msg', function(req, res){
-	headerMsg = req.params.msg;
-	io.emit('header', headerMsg);
+app.get('/admin/serviceLock/:status', function(req, res) {
+    if (req.params.status == "on") {
+        serviceLock = true;
+    } else if (req.params.status == "off") {
+        serviceLock = false;
+    }
 
-	res.end();
+    res.end();
+
 });
 
-io.on('connection', function(socket){
+app.get('/admin/headerMsg/:msg', function(req, res) {
+    headerMsg = req.params.msg;
+    io.emit('header', headerMsg);
 
-	socket.emit('info', infoStatus);
-	socket.emit('header', headerMsg);
+    res.end();
+});
 
-	socket.on('newMessage', function(msg){
+io.on('connection', function(socket) {
 
-		if(msg.body == "/w-num") {
-			io.emit('newMessage', {body:socket.client.conn.server.clientsCount, id:"SRV"});
-			return false;
-		}
+    socket.emit('info', infoStatus);
+    socket.emit('header', headerMsg);
 
-		// HTMLタグを弾く
-		msg.body = msg.body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
+    socket.on('newMessage', function(msg) {
 
-		if (msg.body.length >= 140)
-			return false;
+        if (msg.body == "/w-num") {
+            io.emit('newMessage', {
+                body: socket.client.conn.server.clientsCount,
+                id: "SRV"
+            });
+            return false;
+        }
 
-		io.emit('newMessage', {body: msg.body, id: socket.id, color: msg.color});
+        // HTMLタグを弾く
+        msg.body = msg.body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '');
 
-	});
+        if (msg.body.length >= 140)
+            return false;
 
-	socket.on('iSeeCount', function(msg){
-		iSeeCount++;
-	});
+        io.emit('newMessage', {
+            body: msg.body,
+            id: socket.id,
+            color: msg.color
+        });
 
-	socket.on('pdeFile', function(msg){
+    });
 
-		if (serviceLock) return;
+    socket.on('iSeeCount', function(msg) {
+        iSeeCount++;
+    });
 
-		var pde = msg;
+    socket.on('pdeFile', function(msg) {
 
-		// スケッチファイルに含まれた画像ファイルのパスを抽出
-		var image_paths = pde.match(/[a-zA-Z0-9_\-\.]+\.(jpg|jpeg|png|gif)/ig);
-		if (image_paths == null || image_paths.length == 0) { // 画像ファイルがなければ
-			// ファイルをメモリへ保存し，ファイル情報を配信
-			pdeFile[hashCount] = pde;
-			io.emit('pdeFile', {id: socket.id, hash: hashCount} );
-			hashCount++;
-			return;
-		}
+        if (serviceLock) return;
 
-		// Webからランダムな画像を1つ取得
-		helper.getRandomImage(function (img_buffer) {
+        var pde = msg;
 
-			var image_buffers = [];
-			if (img_buffer != null) { // 取得成功ならば
-				image_buffers[0] = img_buffer;
-				// 使いまわせるようにメモリへ保存
-				pdeImgFiles.push(img_buffer);
-			}
+        // スケッチファイルに含まれた画像ファイルのパスを抽出
+        var image_paths = pde.match(/[a-zA-Z0-9_\-\.]+\.(jpg|jpeg|png|gif)/ig);
+        if (image_paths == null || image_paths.length == 0) { // 画像ファイルがなければ
+            // ファイルをメモリへ保存し，ファイル情報を配信
+            pdeFile[hashCount] = pde;
+            io.emit('pdeFile', {
+                id: socket.id,
+                hash: hashCount
+            });
+            hashCount++;
+            return;
+        }
 
-			// 取得失敗時 or 2枚目以降の画像が必要なならば，既存画像から選ぶ
-			if (1 <= pdeImgFiles.length) { // 既存画像があれば
-				for (var i = image_buffers.length; i < image_paths.length; i++) {
-					image_buffers[i] = pdeImgFiles[Math.floor(Math.random() * pdeImgFiles.length)];
-				}
-			}
+        // Webからランダムな画像を1つ取得
+        helper.getRandomImage(function(img_buffer) {
 
-			// スケッチファイルを書き換え
-			image_paths.forEach(function (path, i) {
-				var img_uri;
-				if (image_buffers[i] != null) { // 選ばれた画像があれば
-					// 画像をBASE64データへ変更
-					img_uri = 'data:image/jpeg;base64,' + image_buffers[i].toString('base64');
-				} else {
-					// OdenToolsロゴへ変更
-					img_uri = 'img/odentools_logo.png';
-				}
-				// 書き換え
-				pde = pde.replace(path, img_uri, 'g');
-			});
+            var image_buffers = [];
+            if (img_buffer != null) { // 取得成功ならば
+                image_buffers[0] = img_buffer;
+                // 使いまわせるようにメモリへ保存
+                pdeImgFiles.push(img_buffer);
+            }
 
-			// ファイルをメモリへ保存し，ファイル情報を配信
-			pdeFile[hashCount] = pde;
-			io.emit('pdeFile', {id: socket.id, hash: hashCount} );
-			hashCount++;
+            // 取得失敗時 or 2枚目以降の画像が必要なならば，既存画像から選ぶ
+            if (1 <= pdeImgFiles.length) { // 既存画像があれば
+                for (var i = image_buffers.length; i < image_paths.length; i++) {
+                    image_buffers[i] = pdeImgFiles[Math.floor(Math.random() * pdeImgFiles.length)];
+                }
+            }
 
-		});
-	});
+            // スケッチファイルを書き換え
+            image_paths.forEach(function(path, i) {
+                var img_uri;
+                if (image_buffers[i] != null) { // 選ばれた画像があれば
+                    // 画像をBASE64データへ変更
+                    img_uri = 'data:image/jpeg;base64,' + image_buffers[i].toString('base64');
+                } else {
+                    // OdenToolsロゴへ変更
+                    img_uri = 'img/odentools_logo.png';
+                }
+                // 書き換え
+                pde = pde.replace(path, img_uri, 'g');
+            });
+
+            // ファイルをメモリへ保存し，ファイル情報を配信
+            pdeFile[hashCount] = pde;
+            io.emit('pdeFile', {
+                id: socket.id,
+                hash: hashCount
+            });
+            hashCount++;
+
+        });
+    });
 
 });
 
 setInterval(function() {
 
-	viewers = io.eio.clientsCount;
+    viewers = io.eio.clientsCount;
 
-	io.emit('graphUupdate', {viewers: viewers, iSeeCount: iSeeCount});
-	iSeeCount = 0;
+    io.emit('graphUupdate', {
+        viewers: viewers,
+        iSeeCount: iSeeCount
+    });
+    iSeeCount = 0;
 
 }, 500);
 
-server.listen(process.env.PORT || 3000, function(){
-	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+/*
+*/
+server.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- www/js/common.js
+++ www/js/common.js
@@ -1,145 +1,252 @@
-$(function () {
+$(function() {
 
-	var socket = io();
 
-	var setMsg = function () {
+		// サーバへ接続
+		var socket = io();
 
-		var msg = new Array();
-		msg[0] = 'できたてホカホカの美味しい ソフトウェアとサービスを。';
-		msg[1] = '”こういうツールがあったら便利かも”というアイデアがあったら是非教えてください。';
-		msg[2] = 'プロジェクトのソースコードを公開しています: <a href="https://github.com/odentools" target="_blank">GitHub</a>';
-		msg[3] = 'ご興味をお持ちの方は、Twitter の <a href="https://twitter.com/odentools" target="_blank">@odentools</a> へお気軽にMentionsをお送りいただければ幸いです！';
+		// 画面を流れるコメント用の設定
+		nicoscreen.start();
+		var droppable = $("#nicoscreen");
 
-		var msgNo = Math.floor(Math.random() * msg.length);
-		$('#msg').html(msg[msgNo]);
-
-	};
-
-	var sendMsg = function () {
-		
-		var textMsg = $('#textBox').val();
-		
-		if (textMsg == "") {
-			return false;
-		}
-
-		if (navigator.platform.indexOf("Win") != -1) {
-
-			for(var i = 0, leni=textMsg.length; i < leni; i++) {
-				if(textMsg.charCodeAt(i) >= 3586 && textMsg.charCodeAt(i) <=3675) {
-					$('#textBox').val('');
-					return false;
-				}
-			}
-			
-		}
-
-		socket.emit('newMessage', { body: textMsg, color: $('#color').val() });
-		$('#textBox').val('');
-		return false;
-
-	};
-	
-	var getTime = function() {
-		
-		var nowTime = new Date();
-		var hour = nowTime.getHours() < 10 ? '0' + nowTime.getHours() : nowTime.getHours();
-		var minute = nowTime.getMinutes() < 10 ? '0' + nowTime.getMinutes() : nowTime.getMinutes();
-		var second = nowTime.getSeconds() < 10 ? '0' + nowTime.getSeconds() : nowTime.getSeconds();
-		
-		return (hour + ':' + minute + ':' + second);
-
-	};
-	
-	var addProcessing = function(hash, id) {
-		var iframeDom = '<iframe src="pde.html?pdeName='+hash+'" name="pfeFile">';
-		$('#chatArea').prepend($('<tr><td>' + getTime() + '<td>' + id + '<td>'+iframeDom+'</tr>'));
-	};
-
-	$('#send').click(function () {
-		if (sendMsg()) return false;
-	});
-
-	$('#textBox').keydown(function (e) {
-		if (e.keyCode === 13) {
-			if (sendMsg()) return false;
-		}
-	});
-
-	$('#iSeeCount').click(function () {
-		socket.emit('iSeeCount');
-	});
-
-	socket.on('newMessage', function (msg) {
-		$('#chatArea').prepend($('<tr><td>' + getTime() + '<td>' + msg.id + '<td>' + msg.body + '</tr>'));
-		nicoscreen.add(msg.body, msg.color);
-	});
-
-	socket.on('graphUupdate', function(msg) {
-		viewers = msg.viewers;
-		iSeeCount = msg.iSeeCount;
-	});
-	
-	socket.on('pdeFile', function(msg) {
-		addProcessing(msg.hash, msg.id);
-	});
-
-	socket.on('info', function(msg) {
-		if (msg == "on") {
-			$('header').css('visibility', 'visible');
-		} else if(msg == "off") {
-			$('header').css('visibility', 'hidden');
-		}
-	});
-
-	socket.on('header', function(msg) {
-		$('header div').html(msg);
-	});
-
-	setMsg();
-	nicoscreen.start();
-
-	var droppable = $("#nicoscreen");
-
-	if (!window.FileReader) {
-		console.log("File API がサポートされていません。");
-		return false;
-	}
-
-	var cancelEvent = function (event) {
-		event.preventDefault();
-		event.stopPropagation();
-		return false;
-	}
-
-	droppable.bind("dragenter", cancelEvent);
-	droppable.bind("dragover", cancelEvent);
-
-	var handleDroppedFile = function (event) {
-		// ファイルは複数ドロップされる可能性がありますが, ここでは 1 つ目のファイルを扱います.
-		var file = event.originalEvent.dataTransfer.files[0];
-
-		var fileReader = new FileReader();
-		fileReader.onload = function (event) {
-
-			var reg=/(.*)(?:\.([^.]+$))/;
-			if( file.name.match(reg)[2] != 'pde')
+		// FileAPIのサポート確認
+		if (!window.FileReader) {
+				console.log("File API がサポートされていません。");
 				return false;
-
-			var source = event.target.result;
-			source = source.replace( /size(.*);/g, "size(500, 500);" );
-
-			socket.emit('pdeFile', source);
-		
 		}
 
-		fileReader.readAsText(file);
 
-		cancelEvent(event);
-		return false;
+		/* ---- */
 
-	}
 
-	droppable.bind("drop", handleDroppedFile);
+		/**
+		 * フッターにランダムなメッセージを設定
+		 */
+    var setMsg = function() {
+
+        var msg = new Array();
+        msg[0] = 'できたてホカホカの美味しい ソフトウェアとサービスを。';
+        msg[1] = '”こういうツールがあったら便利かも”というアイデアがあったら是非教えてください。';
+        msg[2] = 'プロジェクトのソースコードを公開しています: <a href="https://github.com/odentools" target="_blank">GitHub</a>';
+        msg[3] = 'ご興味をお持ちの方は、Twitter の <a href="https://twitter.com/odentools" target="_blank">@odentools</a> へお気軽にMentionsをお送りいただければ幸いです！';
+
+				// ランダムにメッセージ配列のインデックスを選ぶ
+        var msgNo = Math.floor(Math.random() * msg.length);
+        $('#msg').html(msg[msgNo]);
+
+    };
+
+
+		/**
+		 * メッセージの送信処理
+		 */
+    var sendMsg = function() {
+
+				// テキストボックスからメッセージを取得
+        var textMsg = $('#textBox').val();
+
+				// 空白の場合送信中止
+        if (textMsg == "") {
+            return;
+        }
+
+				// Windowsからタイ文字の送信をブロック
+        if (navigator.platform.indexOf("Win") != -1) {
+            for (var i = 0, leni = textMsg.length; i < leni; i++) {
+                if (textMsg.charCodeAt(i) >= 3586 && textMsg.charCodeAt(i) <= 3675) {
+                    $('#textBox').val('');
+                    return;
+                }
+            }
+        }
+
+				// サーバにメッセージを送信
+        socket.emit('newMessage', {
+            body: textMsg,						// メッセージ
+            color: $('#color').val()	// 文字色
+        });
+
+				// メッセージボックスを空にする
+        $('#textBox').val('');
+
+    };
+
+
+		/**
+		 * 現在時刻の取得
+		 */
+    var getTime = function() {
+
+        var nowTime = new Date();
+
+				// 1桁のの場合桁数を2桁の補完
+        var hour = nowTime.getHours() < 10 ? '0' + nowTime.getHours() : nowTime.getHours();
+        var minute = nowTime.getMinutes() < 10 ? '0' + nowTime.getMinutes() : nowTime.getMinutes();
+        var second = nowTime.getSeconds() < 10 ? '0' + nowTime.getSeconds() : nowTime.getSeconds();
+
+        return (hour + ':' + minute + ':' + second);
+
+    };
+
+
+		/**
+		 * Processing実行画面の表示
+		 */
+    var addProcessing = function(hash, id) {
+        var iframeDom = '<iframe src="pde.html?pdeName=' + hash + '" name="pfeFile">';
+
+				// チャットエリアにiframeを埋め込む
+        $('#chatArea').prepend($('<tr><td>' + getTime() + '<td>' + id + '<td>' + iframeDom + '</tr>'));
+    };
+
+
+		/**
+		 * イベントのキャンセル（droppable.bind)
+		 */
+		var cancelEvent = function(event) {
+        event.preventDefault();	// イベントのキャンセル
+        event.stopPropagation();// イベントの伝播をキャンセル
+        return false;
+    }
+
+
+		/**
+		 * ファイルのドロップイベント発生時（droppable.bind)
+		 */
+    var handleDroppedFile = function(event) {
+
+        // ドロップされた1つ目のファイルを指定
+        var file = event.originalEvent.dataTransfer.files[0];
+
+        var fileReader = new FileReader();
+        fileReader.onload = function(event) {
+
+						// 正規表現でファイルの拡張子が'pde'か確認
+            var reg = /(.*)(?:\.([^.]+$))/;
+            if (file.name.match(reg)[2] != 'pde')
+                return false;
+
+						// PDEファイル内のプログラムコードを取得
+            var source = event.target.result;
+
+						// ソースコード中 size(); 命令を強制的に size(500, 500);に書き換
+            source = source.replace(/size(.*);/g, "size(500, 500);");
+
+						// ソースコードをサーバに送信
+            socket.emit('pdeFile', source);
+
+        }
+
+				// ファイル内の文字列の読み込みを開始
+        fileReader.readAsText(file);
+
+				// ドロップイベントのキャンセル
+        cancelEvent(event);
+
+        return false;
+
+    };
+
+
+		/* ---- */
+
+
+		/**
+		 * 送信ボタンクリック時（イベントリスナー）
+		 */
+    $('#send').click(function() {
+        sendMsg();
+				return false;	// clickイベントのキャンセル
+    });
+
+
+		/**
+		 * テキストボックにて，Enter入力時（イベントリスナー）
+		 */
+    $('#textBox').keydown(function(e) {
+				// キーコードがEnterの場合
+        if (e.keyCode === 13)	{
+            sendMsg();
+						return false;
+				}
+    });
+
+
+		/**
+		 * なるほどなぁボタンクリック時
+		 */
+    $('#iSeeCount').click(function() {
+        socket.emit('iSeeCount');
+				return false;	// clickイベントのキャンセル
+    });
+
+
+		/*　----　*/
+
+
+		/**
+		 *　新規メッセージ受信時
+		 */
+    socket.on('newMessage', function(msg) {
+
+				// チャットエリアにメッセージの追加
+        $('#chatArea').prepend($('<tr><td>' + getTime() + '<td>' + msg.id + '<td>' + msg.body + '</tr>'));
+
+				// 画面上を流れるコメントを追加
+        nicoscreen.add(msg.body, msg.color);
+
+    });
+
+
+		/**
+		 * グラフの更新命令受信時
+		 * サーバより，500mS毎に送信
+		 */
+    socket.on('graphUupdate', function(msg) {
+        viewers = msg.viewers;
+        iSeeCount = msg.iSeeCount;
+    });
+
+
+		/**
+		 * PDEファイルの配信受信時
+		 */
+    socket.on('pdeFile', function(msg) {
+				// 読み込むPDEファイルのハッシュ値を指定して，画面に表示する
+        addProcessing(msg.hash, msg.id);
+    });
+
+
+		/**
+		 * 運営メッセージ 表示/非表示 命令受信時
+		 */
+    socket.on('info', function(msg) {
+        if (msg == "on") {
+						// 運営メッセージを表示する
+            $('header').css('visibility', 'visible');
+        } else if (msg == "off") {
+						// 運営メッセージを非表示にする
+            $('header').css('visibility', 'hidden');
+        }
+    });
+
+
+		/**
+		 * 運営メッセージの書き換え命令受信時
+		 */
+    socket.on('header', function(msg) {
+        $('header div').html(msg);
+    });
+
+
+		/* ---- */
+
+		// フッターにランダムなメッセージを表示
+		setMsg();
+
+		// ドロップイベント発生時に呼び出す関数を設定
+		droppable.bind("dragenter", cancelEvent);
+		droppable.bind("dragover", cancelEvent);
+		droppable.bind("drop", handleDroppedFile);
+
 
 });
